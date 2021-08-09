c1201ae5fea143145bb9aca089f74be72f6a966a
Ofir Nemet <ofirnemet@Ofirs-MacBook-Pro.local>
2021-08-09 11:42:42 +0300

    selenium crawler implementation and main start

---
+++ main.py
@@ -0,0 +1,9 @@
+import argparse
+from selenium_crawler import take_screenshot
+
+USER_AGENT = r'msnbot/1.0 (+http://search.msn.com/msnbot.htm)'
+
+argparse.
+
+
+if __name__ == '__main__':
---
+++ selenium_crawler.py
@@ -0,0 +1,21 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def take_screenshot(url: str, user_agent: str, save_path: str) -> str:
+    options: Options = webdriver.ChromeOptions()
+    options.headless = True
+    if user_agent:
+        options.add_argument(f"user-agent={user_agent}")
+    driver = webdriver.Chrome(options=options)
+    
+    driver.get(url)
+    
+
+    S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
+    driver.set_window_size(S('Width'),S('Height'))                                                                                                         
+    driver.find_element_by_tag_name('body').screenshot(save_path)
+    driver.quit()
+
+    return save_path
