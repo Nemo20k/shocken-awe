41b62342c8932da6cf84501544e6fb6ee01efcb0
Ofir Nemet <ofirnemet@Ofirs-MacBook-Pro.local>
2021-08-10 10:53:42 +0300

    start working on pdf option

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 import time
-from selenium_crawler import take_screenshot
+from selenium_crawler import take_screenshot, take_pdf
 
 USER_AGENT = r'msnbot/1.0 (+http://search.msn.com/msnbot.htm)'
 
@@ -12,3 +12,4 @@ parser.add_argument('-s', type=str, dest='save_path', default='screenshot.png')
 if __name__ == '__main__':
     args = parser.parse_args()
     print(take_screenshot(url=args.url, user_agent=USER_AGENT, save_path=args.save_path))
+    #take_pdf(url=args.url, user_agent=USER_AGENT, save_path=args.save_path)
--- selenium_crawler.py
+++ selenium_crawler.py
@@ -1,14 +1,16 @@
-import time
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
+CHROME_DRIVER_PATH = './chromedriver'
 
 def take_screenshot(url: str, user_agent: str, save_path: str) -> str:
     options: Options = webdriver.ChromeOptions()
     options.headless = True
     if user_agent:
         options.add_argument(f"user-agent={user_agent}")
-    driver = webdriver.Chrome(options=options, executable_path='./chromedriver')
+    driver = webdriver.Chrome(options=options, executable_path=CHROME_DRIVER_PATH)
     
     driver.get(url)
     
@@ -18,4 +20,24 @@ def take_screenshot(url: str, user_agent: str, save_path: str) -> str:
     driver.find_element_by_tag_name('body').screenshot(save_path)
     driver.quit()
 
+    return save_path
+
+def take_pdf(url: str, user_agent: str, save_path: str) -> str:
+    options = webdriver.ChromeOptions()
+    settings = {
+        "recentDestinations": [{
+                "id": "Save as PDF",
+                "origin": "local",
+                "account": "",
+            }],
+            "selectedDestinationId": "Save as PDF",
+            "version": 2
+        }
+    prefs = {'printing.print_preview_sticky_settings.appState': json.dumps(settings)}
+    options.add_experimental_option('prefs', prefs)
+    options.add_argument('--kiosk-printing')
+    driver = webdriver.Chrome(chrome_options=options, executable_path=CHROME_DRIVER_PATH)
+    driver.get(url)
+    driver.execute_script('window.print();')
+    driver.quit()
     return save_path
