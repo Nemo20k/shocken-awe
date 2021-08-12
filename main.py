00ee04bd101b9e52952ca0a5fc013d9fbfae152c
Ofir Nemet <ofirnemet@Ofirs-MacBook-Pro.local>
2021-08-12 07:43:08 +0300

    added html save option with flag -html

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import argparse
 import time
 from selenium_crawler import take_screenshot, take_pdf
+from requests_crawler import take_html
 
 USER_AGENT = r'msnbot/1.0 (+http://search.msn.com/msnbot.htm)'
-
 
 parser = argparse.ArgumentParser('shocken-awe')
 parser.add_argument('-url', type=str, dest='url')
 parser.add_argument('-s', type=str, dest='save_path', default='screenshot.png')
+parser.add_argument('-img', action='store_true', dest='screenshot', help='take full page screenshot')
+parser.add_argument('-html', action='store_true', dest='html', help='save html version local')
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    print(take_screenshot(url=args.url, user_agent=USER_AGENT, save_path=args.save_path))
-    #take_pdf(url=args.url, user_agent=USER_AGENT, save_path=args.save_path)
+    if args.screenshot and not args.html:
+        take_screenshot(url=args.url, user_agent=USER_AGENT, save_path=args.save_path)
+    elif args.html:
+        take_html(url=args.url, user_agent=USER_AGENT, save_path=args.save_path)
+    else:
+        print(args.help)
